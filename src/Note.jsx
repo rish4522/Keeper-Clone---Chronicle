4c13b35bfb9095da85d08d0f2d7d943a4e3f1e60
RishEverec <117337128+rish4522@users.noreply.github.com>
2024-01-04 11:13:56 +0530

    Commit

--- src/Note.jsx
+++ src/Note.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import notes from "./notes";
+
+function NoteCard(props) {
+    return (
+        <div className="main-note">
+            <h3>{props.title}</h3>
+            <p>{props.content}</p>
+        </div>
+    );
+}
+
+function createNote(notes) {
+    return (
+        <NoteCard
+            key = {notes.key}
+            title = {notes.title}
+            content = {notes.content}
+        />
+    );
+}
 
 function Note() {
-    return <div className="main-note">
-        <h3>This is the note title</h3>
-        <p>This is the note content</p>
-    </div>
+    return (
+        <>
+            {notes.map(createNote)}
+        </>
+        
+    );
 }
 
 export default Note;
